53fbff2a9a7d4f12134d3ab1a86c61cc3ff602c0
Paddy Allen <Paddy Allen>
2017-05-10 21:49:14 +0100

    hello

--- app.js
+++ app.js
@@ -16,16 +16,12 @@ app.use(bodyParser.json())
 // Index route
 app.get('/', function (req, res) {
     res.send("Hello world, I seem to be working")
-    if (req.query['hub.verify_token'] === 'test-token') {
-      res.send(req.query['hub.challenge']);
-   } else {
-      res.send('Error, wrong validation token');    
-   }
+    
 })
 
 // for Facebook verification
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'test-token') {
+    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
       res.send(req.query['hub.challenge']);
    } else {
       res.send('Error, wrong validation token');    
@@ -37,3 +33,17 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = EAAUhbHLA5tcBANJbmvhA8Hlb58NI4DEHX7rygXMB0sRjpV78pVxBRt6sYxdZAmU2gOEGuVAqM2nmQaChXKY985FjNjWbsYZCCoW1rBuZAbRd3x7cHGAHc9FcX0e8DViYi9oNOINttoGfW0QcdTTDH3lvPL16oAeeLOBpwAPXgZDZD
